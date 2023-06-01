9018e3642724e76516eedf25f31fe777f2a22ed0
Valeriy94-ua <128257210+Valeriy94-ua@users.noreply.github.com>
2023-06-01 16:48:45 +0300

    task 1 complete

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,32 @@
+const refs = {
+  startBtn: document.querySelector('[data-start]'),
+  stopBtn: document.querySelector('[data-stop]'),
+  body: document.querySelector('body'),
+};
 
+const COLOR_DELAY_CHANGE = 1000;
+let setInt = null;
+
+// random color
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
+}
+
+// setting start btn
+refs.startBtn.addEventListener('click', () => {
+  refs.startBtn.disabled = true;
+  refs.stopBtn.disabled = false;
+
+  setInt = setInterval(() => {
+    refs.body.style.backgroundColor = getRandomHexColor();
+  }, COLOR_DELAY_CHANGE);
+});
+
+// setting stop btn
+refs.stopBtn.addEventListener('click', () => {
+  clearInterval(setInt);
+  refs.stopBtn.disabled = true;
+  refs.startBtn.disabled = false;
+});
