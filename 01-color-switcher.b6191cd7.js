a6141007f9c2b619a5604dd916ff3101d907def1
Valeriy94-ua <128257210+Valeriy94-ua@users.noreply.github.com>
2023-06-03 12:12:07 +0000

    Deploying to gh-pages from @ Valeriy94-ua/goit-js-hw-09@01409a064c036c22bfc379fc38a43992ca8651e0 🚀

---
+++ 01-color-switcher.b6191cd7.js
@@ -0,0 +1,2 @@
+const t={startBtn:document.querySelector("[data-start]"),stopBtn:document.querySelector("[data-stop]"),body:document.querySelector("body")};let e=null;t.startBtn.addEventListener("click",(()=>{t.startBtn.disabled=!0,t.stopBtn.disabled=!1,e=setInterval((()=>{t.body.style.backgroundColor=`#${Math.floor(16777215*Math.random()).toString(16).padStart(6,0)}`}),1e3)})),t.stopBtn.addEventListener("click",(()=>{clearInterval(e),t.stopBtn.disabled=!0,t.startBtn.disabled=!1}));
+//# sourceMappingURL=01-color-switcher.b6191cd7.js.map
